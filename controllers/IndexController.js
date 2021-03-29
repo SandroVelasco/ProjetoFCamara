c247e8d1d0b2689f9d31a5b64199391bf045cafb
Sandro Rohan <40503535+SandroVelasco@users.noreply.github.com>
2021-03-29 20:36:02 -0300

    Update IndexController.js

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -15,13 +15,13 @@ const IndexController = {
     res.render('./../views/fazer-doacao-2');
   },
 
-    /**
- * Rota: /confirmacao 
- * Método: GET
- */
+  /**
+   * Rota: /confirmacao 
+   * Método: GET
+   */
   confirmacao: (req, res) => {
     res.render('./../views/confirmacao.ejs');
-},
+  },
 
   /**
    * Rota: /pagamento 
@@ -30,7 +30,14 @@ const IndexController = {
   pagamento: (req, res) => {
     res.render('./../views/pagamento.ejs');
   }
-
+  
+  /**
+   * Rota: /agradecimento
+   * Método: GET
+   */
+  agradecimento: (req, res) => {
+    res.render('./../views/agradecimento');
+  }
 }
 
 module.exports = IndexController;
