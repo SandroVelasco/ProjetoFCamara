77d68c59799ba16e84e42778550d72e7e01e6aa9
SdRohan <sandrorohan@hotmail.com>
2021-03-30 13:59:00 -0300

    Alteração para modelo de classe

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -1,43 +1,45 @@
-const IndexController = {
+class IndexController {
+  constructor() {}
+
   /**
    * Rota: /
    * Método: GET
    */
-  index: (req, res) => {
-    res.render('./../views/index');
-  },
+  index(req, res) {
+    res.render("./../views/index");
+  }
 
   /**
-   * Rota: /doacao2
+   * Rota: /doacao
    * Método: GET
    */
-  doacao2: (req, res) => {
+  doacao(req, res) {
     res.render('./../views/fazer-doacao-2');
-  },
+  }
 
   /**
    * Rota: /confirmacao 
    * Método: GET
    */
-  confirmacao: (req, res) => {
+  confirmacao(req, res) {
     res.render('./../views/confirmacao.ejs');
-  },
+  }
 
   /**
    * Rota: /pagamento 
    * Método: GET
    */
-  pagamento: (req, res) => {
+  pagamento(req, res) {
     res.render('./../views/pagamento');
-  },
+  }
 
   /**
    * Rota: /agradecimento
    * Método: GET
    */
-  agradecimento: (req, res) => {
+  agradecimento(req, res) {
     res.render('./../views/agradecimento');
   }
 }
 
-module.exports = IndexController;
+module.exports = new IndexController();
